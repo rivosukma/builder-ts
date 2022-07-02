22a4a596092687c50fccc9d1ef18320e293c116a
rivo putra <rivo.putra@klb.co.id>
2022-07-02 14:34:22 +0700

    sidebar remove props

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Props } from "../../pages/react";
 
-const Drawer: React.FC<Props> = ({ children }) => {
+const Drawer: React.FC<Props> = () => {
   return (
     <aside className="drawer-side h-screen p-4 overflow-y-auto w-80 text-base-content">
       <ul className="menu">
